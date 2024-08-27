d1e353de4747fd9705fc24d347f12753746b9427
timur_malikov_GITD <timur.malikov@visier.com>
2024-08-26 17:13:05 -0700

    [VAN-125939] Created core package with common logic for all APIs.

---
+++ src/visier.sdk.api.core/visier/sdk/api/core/__init__.py
@@ -0,0 +1,10 @@
+# import ApiClient
+from visier.sdk.api.core.api_response import ApiResponse
+from visier.sdk.api.core.api_client import ApiClient
+from visier.sdk.api.core.configuration import Configuration
+from visier.sdk.api.core.exceptions import OpenApiException
+from visier.sdk.api.core.exceptions import ApiTypeError
+from visier.sdk.api.core.exceptions import ApiValueError
+from visier.sdk.api.core.exceptions import ApiKeyError
+from visier.sdk.api.core.exceptions import ApiAttributeError
+from visier.sdk.api.core.exceptions import ApiException
