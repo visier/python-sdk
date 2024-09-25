38574da97cdd2912a755f7b90dd5425ab9304119
Visier-Build-Team <action@github.com>
2024-09-25 23:36:00 +0000

    Autogenerated Python SDK

--- src/visier_api_analytic_model/setup.py
+++ src/visier_api_analytic_model/setup.py
@@ -23,10 +23,10 @@ from setuptools import setup, find_packages  # noqa: H301
 # prerequisite: setuptools
 # http://pypi.python.org/pypi/setuptools
 NAME = "visier-api-analytic-model"
-VERSION = "0.99201.1494"
+VERSION = "0.99201.1494.beta2"
 PYTHON_REQUIRES = ">=3.8"
 REQUIRES = [
-    "visier-api-core ~= 0.99201.1494",
+    "visier-api-core ~= 0.99201.1494.beta2",
 ]
 
 setup(
